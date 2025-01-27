c34686983d563675bb9cfc7d36340740bbd34271
Anket Kohak <anketkohak40@gmail.com>
2025-01-27 23:13:32 +0530

    Adding Expense to a Budget

--- BudgetApp/Screens/BudgetDetailScreen.swift
+++ BudgetApp/Screens/BudgetDetailScreen.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct BudgetDetailScreen: View {
     let budget: Budget
-    
+    @Environment(\.managedObjectContext) private var context
     @State private var title: String = ""
     @State private var amount: Double?
     private var isFormValid :Bool{
         !title.isEmptyOrWhitespace && amount != nil && Double(amount!)>0
+    }
+    private func addExpense(){
+        let expense = Expense(context: context)
+        expense.title = title
+        expense.amount = amount ?? 0
+        expense.dateCreated = Date()
+        budget.addToExpenses(expense)
+        do{
+            try context.save()
+            title = ""
+            amount = nil
+        }catch{
+            print(error.localizedDescription)
+        }
     }
     var body: some View {
         Form{
@@ -23,7 +37,7 @@ struct BudgetDetailScreen: View {
                     .keyboardType(.numberPad)
                 
                 Button{
-                    
+                    addExpense()
                 }label: {
                     Text("Save")
                         .frame(maxWidth: .infinity)
