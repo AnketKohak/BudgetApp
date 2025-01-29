10ce3c474a1fa38842a902e8b165abc370d1dfca
Anket Kohak <anketkohak40@gmail.com>
2025-01-29 13:42:45 +0530

    Implementing Sorting

--- BudgetApp/Screens/FilterScreen.swift
+++ BudgetApp/Screens/FilterScreen.swift
@@ -13,14 +13,81 @@ struct FilterScreen: View {
     @FetchRequest(sortDescriptors: []) private var expenses: FetchedResults<Expense>
     
     @State private var selectedTags: Set<Tag> = []
+    
+    @State private var selectedSortOption:SortOptions? = nil
+    @State private var selectedSortDirection:SortDirection = .asc
+    
     @State private var filterdExpenses: [Expense] = []
     @State private var showAll: Bool = false
     
     @State private var startPrice: Double?
     @State private var endPrice: Double?
     
+    @State private var title: String = ""
     
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    
+    
+    // MARK: - Enum
+    private enum SortOptions: CaseIterable,Identifiable{
+        case title
+        case date
+        
+        var id :SortOptions{
+            return self
+        }
+        
+        var title:String{
+            switch self{
+            case .title:
+                return "Title"
+            case .date:
+                return "Date"
+            }
+        }
+        
+        var key:String{
+            switch self{
+            case .title:
+                return "title"
+            case .date:
+                return "dateCreated"
+            }
+        }
+        
+    }
+    private enum SortDirection: CaseIterable,Identifiable{
+        case asc
+        case desc
+        
+        var id :SortDirection{
+            return self
+        }
+        
+        var title:String{
+            switch self{
+            case .asc:
+                return "Asending"
+            case .desc:
+                return "Descending"
+            }
+        }
+        
+    }
     // MARK: - functions
+    private func  performSort(){
+        guard let sortOption = selectedSortOption else { return }
+        let request = Expense.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key : sortOption.key, ascending: selectedSortDirection == .asc ? true: false)]
+        
+        do{
+            filterdExpenses = try context.fetch(request)
+        }catch{
+            print(error)
+        }
+    }
+    
     private func filterTags(){
         if selectedTags.isEmpty{
             return
@@ -49,10 +116,59 @@ struct FilterScreen: View {
             print(error)
         }
     }
+    private  func filterByTitle(){
+        if !title.isEmpty{
+            let request = Expense.fetchRequest()
+            request.predicate = NSPredicate(format: "title BEGINSWITH %@", NSString(string: title))
+            
+            do{
+                filterdExpenses = try context.fetch(request)
+            }catch{
+                print(error)
+            }
+            
+        }
+    }
+    private func filterByDate(){
+        let request = Expense.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "dateCreated >= %@ AND dateCreated <= %@", startDate as NSDate, endDate as NSDate)
+        
+        do{
+            filterdExpenses = try context.fetch(request)
+        }catch{
+            print(error)
+        }
+    }
     // MARK: - Body
     
     var body: some View {
-        VStack(alignment: .leading){ //parent stack
+        List{ //parent stack
+            // MARK: - Sorting
+            // MARK:  SortOption
+            Section("Sort"){
+                Picker("Sort Option", selection: $selectedSortOption){
+                    Text("Select").tag(Optional<SortOptions>(nil))
+                    ForEach(SortOptions.allCases){option in
+                        Text(option.title)
+                            .tag(Optional(option))
+                    }
+                }
+                // MARK: SortDirection
+                Picker("Sort Direction",selection: $selectedSortDirection){
+                    ForEach(SortDirection.allCases){ option in
+                        Text(option.title)
+                            .tag(option)
+                    }
+                }
+                HStack{
+                    Spacer()
+                    Button("Sort"){
+                        performSort()
+                    }.buttonStyle(.borderless)
+                    Spacer()
+                }
+            }
             // MARK: Tags
             Section("Filter by Tags"){
                 TagsView(selectedTags: $selectedTags)
@@ -64,20 +180,40 @@ struct FilterScreen: View {
                         .textFieldStyle(.roundedBorder)
                     TextField("End Price", value: $endPrice, format: .number)
                         .textFieldStyle(.roundedBorder)
-
+                    
                     Button("Search"){
                         filterByPrice()
                     }
                 }
             }
-            // MARK: Display
-            List(filterdExpenses){ expense in
-                ExpenseCellView(expense: expense)
+            Section("Filter By title"){
+                HStack{
+                    TextField("Enter title",text: $title)
+                        .textFieldStyle(.roundedBorder)
+                    Button("Search"){
+                        filterByTitle()
+                    }
+                }
+                
             }
-            Spacer()
+            Section("Filter by Date"){
+                DatePicker("Start Date" ,selection: $startDate,displayedComponents: .date)
+                DatePicker("End Date" ,selection: $endDate,displayedComponents: .date)
+                Button("Search"){
+                    filterByDate()
+                }
+            }
+            
+            // MARK: Display
+            Section("Expenses"){
+                ForEach(filterdExpenses){ expense in
+                    ExpenseCellView(expense: expense)
+                }
+            }
+            
             HStack{
-                Spacer()
                 // MARK: ShowAll
+                Spacer()
                 Button("Show All"){
                     selectedTags = []
                     filterdExpenses = expenses.map{$0}
